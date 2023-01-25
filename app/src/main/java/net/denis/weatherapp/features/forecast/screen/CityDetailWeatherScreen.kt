8ce36bffe75c114813779a32d95a4eadc59f7e91
den-android <irethegreat@yandex.ru>
2023-01-25 08:43:09 +0300

    Update CityDetailWeatherScreen.kt

--- app/src/main/java/net/denis/weatherapp/features/forecast/screen/CityDetailWeatherScreen.kt
+++ app/src/main/java/net/denis/weatherapp/features/forecast/screen/CityDetailWeatherScreen.kt
@@ -1,17 +1,25 @@
 package net.denis.weatherapp.features.forecast.screen
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import net.denis.weatherapp.core.data.datasource.remote.dto.weather_forecast.WeatherDto
 import net.denis.weatherapp.features.forecast.mvi.ForecastViewModel
+import net.denis.weatherapp.features.forecast.screen.components.compose_items.CellWithText
 
 @Composable
 fun CityDetailWeatherScreen(
     vm: ForecastViewModel,
+    currentCnt: Int,
 ) {
     val state = vm.viewState.collectAsState()
     val weather = state.value.weather
 
+    weather?.let { weatherItem ->
+        DetailItems(weatherDto = weatherItem)
+    }
 
 
 }
@@ -19,7 +27,18 @@ fun CityDetailWeatherScreen(
 @Composable
 private fun DetailItems(
     modifier: Modifier = Modifier,
-
+    weatherDto: WeatherDto,
 ) {
-
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth()
+        ) {
+            CellWithText()
+            Spacer(modifier = modifier.width(16.dp))
+            CellWithText()
+        }
+    }
 }
