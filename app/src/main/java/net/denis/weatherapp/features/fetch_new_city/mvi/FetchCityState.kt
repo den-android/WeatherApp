40b263505d96eedcca41dddef750a5fb57797b8f
den-android <irethegreat@yandex.ru>
2023-02-07 14:01:12 +0300

    ref - removed unused imports

--- app/src/main/java/net/denis/weatherapp/features/fetch_new_city/mvi/FetchCityState.kt
+++ app/src/main/java/net/denis/weatherapp/features/fetch_new_city/mvi/FetchCityState.kt
@@ -1,7 +1,6 @@
 package net.denis.weatherapp.features.fetch_new_city.mvi
 
 import net.denis.weatherapp.core.presentation.redux.State
-import net.denis.weatherapp.features.fetch_new_city.model.FetchCityData
 
 data class FetchCityState(
     val isLoading: Boolean = false,
