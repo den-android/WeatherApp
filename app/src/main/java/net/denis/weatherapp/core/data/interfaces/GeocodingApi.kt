0cd45f9c6145725cbbb61349cde5c3a79d9772b1
den-android <irethegreat@yandex.ru>
2023-02-07 13:16:11 +0300

    Update GeocodingApi.kt

--- app/src/main/java/net/denis/weatherapp/core/data/interfaces/GeocodingApi.kt
+++ app/src/main/java/net/denis/weatherapp/core/data/interfaces/GeocodingApi.kt
@@ -1,5 +1,17 @@
 package net.denis.weatherapp.core.data.interfaces
 
+import net.denis.weatherapp.core.data.datasource.remote.dto.geocoding.GeocodingDto
+import net.denis.weatherapp.core.util.Constants.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
 interface GeocodingApi {
 
+    @GET("direct?q=name,ru&limit=10&appid=${API_KEY}")
+    @Headers("Content-type: application/json")
+    suspend fun fetchCity(
+        @Query("city name") name: String,
+    ) : Response<GeocodingDto>
 }
