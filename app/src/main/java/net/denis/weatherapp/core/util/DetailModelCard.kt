558a437d49fad9c71745862b63d06ba290d8abd0
den-android <irethegreat@yandex.ru>
2023-02-08 17:42:55 +0300

    Update DetailModelCard.kt

--- app/src/main/java/net/denis/weatherapp/core/util/DetailModelCard.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/DetailModelCard.kt
@@ -2,27 +2,27 @@ package net.denis.weatherapp.core.util
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 import net.denis.weatherapp.features.detail_forecast.model.CellFields
 import net.denis.weatherapp.features.detail_forecast.model.IndicatorCellFields
 
 @Parcelize
-sealed class DetailModelCard: Parcelable {
+sealed class DetailModelCard : Parcelable {
 
-    data class WideCardWithText(val indicatorCellFields: IndicatorCellFields) : DetailModelCard()
+    data class WideCardWithText(val indicatorCellFields: IndicatorCellFields) : DetailModelCard() {
 
-    data class CardWithText(val cellFields: CellFields) : DetailModelCard()
+        data class CardWithText(val cellFields: CellFields) : DetailModelCard()
 
-    data class CardWithIndicator(val indicatorCellFields: IndicatorCellFields) : DetailModelCard()
+        data class CardWithIndicator(val indicatorCellFields: IndicatorCellFields) : DetailModelCard()
 
-    data class CardWithTextAndIndicator(
-        val cardWithText: CardWithText,
-        val cardWithIndicator: CardWithIndicator,
-    ) : DetailModelCard()
-    
-    data class CardWithTextAndText(
-        val leftCardWithText: CardWithText,
-        val rightCardWithText: CardWithText,
-    ) : DetailModelCard()
+        data class CardWithTextAndIndicator(
+            val cardWithText: CardWithText,
+            val cardWithIndicator: CardWithIndicator,
+        ) : DetailModelCard()
+
+        data class CardWithTextAndText(
+            val leftCardWithText: CardWithText,
+            val rightCardWithText: CardWithText,
+        ) : DetailModelCard()
+    }
 
 }
