fa735779a5afe86778d6ec30f85de224a7ae7217
den-android <irethegreat@yandex.ru>
2023-02-08 13:31:47 +0300

    return api key

--- app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
@@ -5,5 +5,6 @@ import net.denis.weatherapp.BuildConfig
 object Constants {
     const val PARAM_TO_DETAIL_SCREEN = "detailData"
     const val PARAM_TO_MAIN_SCREEN = "cityData"
-    const val API_KEY = BuildConfig.API_KEY
+    const val PRIVATE_API_KEY = BuildConfig.API_KEY
+    const val API_KEY = "b05865d24d90b1dbccfb3ced2627b4e9"
 }
