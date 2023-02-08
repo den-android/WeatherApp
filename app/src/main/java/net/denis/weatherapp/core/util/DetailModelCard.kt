0ff81d2d25e402508f86495211734a0353682a43
den-android <irethegreat@yandex.ru>
2023-02-08 17:47:22 +0300

    Update DetailModelCard.kt
    ref

--- app/src/main/java/net/denis/weatherapp/core/util/DetailModelCard.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/DetailModelCard.kt
@@ -8,7 +8,7 @@ import net.denis.weatherapp.features.detail_forecast.model.IndicatorCellFields
 @Parcelize
 sealed class DetailModelCard : Parcelable {
 
-    data class WideCardWithText(val indicatorCellFields: IndicatorCellFields) : DetailModelCard() {
+    data class WideCardWithText(val indicatorCellFields: IndicatorCellFields) : DetailModelCard()
 
         data class CardWithText(val cellFields: CellFields) : DetailModelCard()
 
@@ -23,6 +23,5 @@ sealed class DetailModelCard : Parcelable {
             val leftCardWithText: CardWithText,
             val rightCardWithText: CardWithText,
         ) : DetailModelCard()
-    }
 
 }
