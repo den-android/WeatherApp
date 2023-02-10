706b505b12a31fa8200651b9faec8dfd1b527406
den-android <irethegreat@yandex.ru>
2023-02-10 12:17:29 +0300

    fix NavGraph logic
    removed savedStatehandle, move this logic into mvi

--- app/src/main/java/net/denis/weatherapp/core/presentation/navigation/NavGraph.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/navigation/NavGraph.kt
@@ -7,11 +7,9 @@ import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
-import net.denis.weatherapp.core.util.Constants.PARAM_TO_DETAIL_SCREEN
-import net.denis.weatherapp.core.util.Constants.PARAM_TO_MAIN_SCREEN
+import net.denis.weatherapp.core.util.Constants.PARAM_POSITION
 import net.denis.weatherapp.features.detail_forecast.mvi.DetailViewModel
 import net.denis.weatherapp.features.detail_forecast.screen.DetailWeatherScreen
-import net.denis.weatherapp.features.fetch_new_city.model.CityData
 import net.denis.weatherapp.features.fetch_new_city.mvi.FetchCityViewModel
 import net.denis.weatherapp.features.fetch_new_city.screen.FetchCityScreen
 import net.denis.weatherapp.features.main_forecast.mvi.MainViewModel
@@ -25,44 +23,35 @@ fun NavGraph(
     fetchCityVM: FetchCityViewModel,
 ) {
     val mainState = mainVM.viewState.collectAsState().value.forecastData
+    val cityState = fetchCityVM.viewState.collectAsState().value.cityData
 
     NavHost(
         navController = navController,
-        startDestination = Screen.MainForecastScreen.route
+        startDestination = Screen.MainScreen.route
     ) {
 
         composable(
-            route = Screen.MainForecastScreen.route,
-        ) { navBackStackEntry ->
-
-            val cityDataItem =
-                navController.previousBackStackEntry?.savedStateHandle?.get<CityData>(
-                    PARAM_TO_MAIN_SCREEN
-                )
-
-            cityDataItem?.let {
-                mainVM.fetchForecastByCityCoords(lat = it.lat, lon = it.lon)
-                MainScreen(navController = navController, vm = mainVM)
-            } ?: mainVM.fetchForecastByCityCoords(lat = 47.2213858, lon = 39.7114196).also {
-                MainScreen(navController = navController, vm = mainVM)
-            }
-
+            route = Screen.MainScreen.route
+        ) {
+            cityState?.let {
+                mainVM.fetchForecastByCityCoords(lat = it.lat, lon = it.lon).also {
+                    MainScreen(navController = navController, vm = mainVM)
+                }
+            } ?: MainScreen(navController = navController, vm = mainVM)
         }
 
         composable(
-            route = Screen.DetailForecastScreen.route,
-            arguments = listOf(navArgument(PARAM_TO_DETAIL_SCREEN) {
+            route = Screen.DetailScreen.route,
+            arguments = listOf(navArgument(PARAM_POSITION) {
                 type = NavType.IntType
             })
         ) { navBackStackEntry ->
-
-            navBackStackEntry.arguments?.getInt(PARAM_TO_DETAIL_SCREEN)?.let { position ->
+            navBackStackEntry.arguments?.getInt(PARAM_POSITION)?.let { position ->
                 mainState?.let { forecastData ->
                     detailVM.getDetailData(forecastData.forecastList[position].detailData)
                 }
                 DetailWeatherScreen(vm = detailVM)
             }
-
         }
 
         composable(route = Screen.FetchCityScreen.route) {
