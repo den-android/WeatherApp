fe7a0723ee829b70228ffd2b0bda7d84a36f2f06
den-android <irethegreat@yandex.ru>
2023-02-10 11:05:51 +0300

    Update Constants.kt
    replace object to lat,lon params

--- app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
@@ -4,7 +4,8 @@ import net.denis.weatherapp.BuildConfig
 
 object Constants {
     const val PARAM_TO_DETAIL_SCREEN = "position"
-    const val PARAM_TO_MAIN_SCREEN = "cityData"
+    const val PARAM_LAT = "lat"
+    const val PARAM_LON = "lon"
     const val PRIVATE_API_KEY = BuildConfig.API_KEY
     const val API_KEY = "b05865d24d90b1dbccfb3ced2627b4e9"
 }
