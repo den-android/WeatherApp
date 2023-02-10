606a2879833532f4e769a31d0ac4a0ef0e64e47f
den-android <irethegreat@yandex.ru>
2023-02-10 12:19:28 +0300

    removed shimmer item

--- app/src/main/java/net/denis/weatherapp/features/main_forecast/screen/MainScreen.kt
+++ app/src/main/java/net/denis/weatherapp/features/main_forecast/screen/MainScreen.kt
@@ -13,7 +13,6 @@ import net.denis.weatherapp.core.presentation.ui.theme.CityBackground
 import net.denis.weatherapp.features.main_forecast.mvi.MainViewModel
 import net.denis.weatherapp.features.main_forecast.screen.components.BottomNavigateMenu
 import net.denis.weatherapp.features.main_forecast.screen.components.CurrentWeatherDisplay
-import net.denis.weatherapp.features.main_forecast.screen.components.ShimmerMainScreen
 import net.denis.weatherapp.features.main_forecast.screen.components.WeatherForecastDisplay
 
 @Composable
@@ -25,41 +24,40 @@ fun MainScreen(
     val state = vm.viewState.collectAsState()
     val mainState = state.value.forecastData
 
-    ShimmerMainScreen(isLoading = !state.value.isLoading) {
-        mainState?.forecastList?.let { itemForecast ->
-            Column(
-                modifier = modifier
-                    .fillMaxSize()
-                    .background(color = CityBackground)
-            ) {
-                Box(modifier = modifier.weight(3f)) {
-                    CurrentWeatherDisplay(
-                        city = mainState.cityDetail.cityName,
-                        weatherIcon = itemForecast[0].meteorology[0].id,
-                        temp = itemForecast[0].temp,
-                        weatherDesc = itemForecast[0].meteorology[0].description,
-                        currentDateTime = itemForecast[0].dateTime
-                    )
-                }
-
-                Box(modifier = modifier.weight(1f)) {
-                    WeatherForecastDisplay(
-                        forecastData = mainState,
-                        onClick = {
-                            navController.navigate(
-                                route = Screen.DetailScreen.passDetailPosition(it)
-                            )
-                        }
-                    )
-                }
-
-                BottomNavigateMenu(onFabClicked = {
-                    navController.navigate(
-                        route = Screen.FetchCityScreen.route
-                    )
-                })
+    mainState?.forecastList?.let { itemForecast ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .background(color = CityBackground)
+        ) {
+            Box(modifier = modifier.weight(3f)) {
+                CurrentWeatherDisplay(
+                    city = mainState.cityDetail.cityName,
+                    weatherIcon = itemForecast[0].meteorology[0].id,
+                    temp = itemForecast[0].temp,
+                    weatherDesc = itemForecast[0].meteorology[0].description,
+                    currentDateTime = itemForecast[0].dateTime
+                )
             }
+
+            Box(modifier = modifier.weight(1f)) {
+                WeatherForecastDisplay(
+                    forecastData = mainState,
+                    onClick = {
+                        navController.navigate(
+                            route = Screen.DetailScreen.passDetailPosition(it)
+                        )
+                    }
+                )
+            }
+
+            BottomNavigateMenu(onFabClicked = {
+                navController.navigate(
+                    route = Screen.FetchCityScreen.route
+                )
+            })
         }
     }
+
 
 }
