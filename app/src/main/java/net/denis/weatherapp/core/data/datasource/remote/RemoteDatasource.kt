32aa36de4e2e36fb2eca4b6c47ed6e1cfaa154e1
den-android <irethegreat@yandex.ru>
2023-02-16 17:00:53 +0300

    rollback changes at remote data source

--- app/src/main/java/net/denis/weatherapp/core/data/datasource/remote/RemoteDatasource.kt
+++ app/src/main/java/net/denis/weatherapp/core/data/datasource/remote/RemoteDatasource.kt
@@ -1,15 +1,12 @@
 package net.denis.weatherapp.core.data.datasource.remote
 
-import android.util.Log
 import net.denis.weatherapp.core.data.datasource.remote.dto.geocoding.GeocodingDto
 import net.denis.weatherapp.core.data.datasource.remote.dto.weather_forecast.WeatherDto
 import net.denis.weatherapp.core.data.interfaces.GeocodingApi
 import net.denis.weatherapp.core.data.interfaces.IRemoteDatasource
 import net.denis.weatherapp.core.data.interfaces.OpenWeatherApi
 import net.denis.weatherapp.core.util.NetworkResult
-import net.denis.weatherapp.core.util.getResult
 import net.denis.weatherapp.core.util.handleApi
-import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteDatasource @Inject constructor(
@@ -17,27 +14,12 @@ class RemoteDatasource @Inject constructor(
     private val geocodingApi: GeocodingApi,
 ) : IRemoteDatasource {
 
-    override suspend fun fetchForecastByCoords(lat: Double, lon: Double): Result<WeatherDto> {
-        val response = handleApi { openWeatherApi.fetchForecastByCity(lat, lon) }
-        return when (response) {
-            is NetworkResult.Success -> {
-                getResult { response.data }
-            }
-
-            is NetworkResult.Error -> {
-                getResult { response.message }
-                //Log.d("Logging", "ERROR: ${response.errorMessage}\n")
-            }
-
-            is NetworkResult.Exception -> {
-                getResult { response.e.localizedMessage }
-                Log.d("App crash", "${response.e.localizedMessage}")
-            }
-        }
+    override suspend fun fetchForecastByCoords(lat: Double, lon: Double): NetworkResult<WeatherDto> {
+        return handleApi { openWeatherApi.fetchForecastByCity(lat = lat, lon = lon) }
     }
 
     override suspend fun fetchCity(name: String): NetworkResult<GeocodingDto> {
-        return handleApi { geocodingApi.fetchCity(name) }
+        return handleApi { geocodingApi.fetchCity(name = name) }
     }
 
 }
