8f93e137afbe1bf5dd0bb2cbdad97863fe4e2ef5
den-android <irethegreat@yandex.ru>
2023-02-16 17:23:14 +0300

    Update IWeatherRepository.kt

--- app/src/main/java/net/denis/weatherapp/core/data/interfaces/IWeatherRepository.kt
+++ app/src/main/java/net/denis/weatherapp/core/data/interfaces/IWeatherRepository.kt
@@ -2,10 +2,9 @@ package net.denis.weatherapp.core.data.interfaces
 
 import kotlinx.coroutines.flow.Flow
 import net.denis.weatherapp.core.data.datasource.remote.dto.weather_forecast.WeatherDto
+import net.denis.weatherapp.core.util.ErrorHandler
+import net.denis.weatherapp.core.util.NetworkResult
 
 interface IWeatherRepository {
-    suspend fun fetchForecast(
-        lat: Double,
-        lon: Double,
-    ): Flow<WeatherDto>
+    suspend fun fetchForecast(lat: Double, lon: Double): Flow<NetworkResult<WeatherDto>>
 }
