1019db30243dadc9202b4634baf29a03267385cf
den-android <irethegreat@yandex.ru>
2023-02-16 17:02:15 +0300

    Update NetworkResult.kt
    deleted handle api fun

--- app/src/main/java/net/denis/weatherapp/core/util/NetworkResult.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/NetworkResult.kt
@@ -1,28 +1,15 @@
 package net.denis.weatherapp.core.util
 
-import retrofit2.HttpException
-import retrofit2.Response
+sealed interface ErrorHandler<T: Any>
 
-sealed class NetworkResult<T : Any> {
-    class Success<T: Any>(val data: T) : NetworkResult<T>()
-    class Error<T: Any>(val code: Int, val message: String?) : NetworkResult<T>()
-    class Exception<T: Any>(val e: Throwable) : NetworkResult<T>()
+sealed class NetworkResult<T : Any>: ErrorHandler<T> {
+    class Success<T : Any>(val data: T) : NetworkResult<T>()
+    class Failure<T : Any>(val code: Int, val message: String) : NetworkResult<T>()
+    class Exception<T : Any>(val e: kotlin.Exception) : NetworkResult<T>()
 }
 
-suspend fun <T : Any> handleApi(
-    execute: suspend () -> Response<T>
-): NetworkResult<T> {
-    return try {
-        val response = execute()
-        val body = response.body()
-        if (response.isSuccessful && body != null) {
-            NetworkResult.Success(body)
-        } else {
-            NetworkResult.Error(code = response.code(), message = response.message())
-        }
-    } catch (e: HttpException) {
-        NetworkResult.Error(code = e.code(), message = e.message())
-    } catch (e: Throwable) {
-        NetworkResult.Exception(e)
-    }
+sealed class SomeResult<T: Any>: ErrorHandler<T> {
+    class Success<T : Any>(val data: T) : SomeResult<T>()
+    class Failure<T : Any>(val message: String) : SomeResult<T>()
+    class Exception<T : Any>(val e: kotlin.Exception) : SomeResult<T>()
 }
