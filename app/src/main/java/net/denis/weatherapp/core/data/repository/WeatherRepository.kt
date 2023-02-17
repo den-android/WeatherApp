a020634a77dbbd897f39d2ba52c6df6336127676
den-android <irethegreat@yandex.ru>
2023-02-17 16:23:02 +0300

    add coroutine scope

--- app/src/main/java/net/denis/weatherapp/core/data/repository/WeatherRepository.kt
+++ app/src/main/java/net/denis/weatherapp/core/data/repository/WeatherRepository.kt
@@ -1,25 +1,27 @@
 package net.denis.weatherapp.core.data.repository
 
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.withContext
 import net.denis.weatherapp.core.data.datasource.remote.dto.weather_forecast.WeatherDto
 import net.denis.weatherapp.core.data.interfaces.ILocalDatasource
 import net.denis.weatherapp.core.data.interfaces.IRemoteDatasource
 import net.denis.weatherapp.core.data.interfaces.IWeatherRepository
-import net.denis.weatherapp.core.util.ErrorHandler
 import net.denis.weatherapp.core.util.NetworkResult
 import javax.inject.Inject
 
 class WeatherRepository @Inject constructor(
+    private val scopeIo: CoroutineScope,
     private val localDatasource: ILocalDatasource,
-    private val remoteDatasource: IRemoteDatasource,
+    private val remoteDatasource: IRemoteDatasource
 ) : IWeatherRepository {
 
-    override suspend fun fetchForecast(lat: Double, lon: Double): Flow<NetworkResult<WeatherDto>> {
-        val response = remoteDatasource.fetchForecastByCoords(lat = lat, lon = lon)
-        return flow {
-            emit(value = response)
+    override suspend fun fetchForecast(lat: Double, lon: Double): Flow<NetworkResult<WeatherDto>> =
+        withContext(scopeIo.coroutineContext) {
+            val response = remoteDatasource.fetchForecastByCoords(lat = lat, lon = lon)
+            return@withContext flow {
+                emit(value = response)
+            }
         }
-    }
-
 }
