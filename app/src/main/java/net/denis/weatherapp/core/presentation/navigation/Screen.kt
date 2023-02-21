a74fc656c823a2e7241359cd4b1ede2a8dbdb65e
den-android <irethegreat@yandex.ru>
2023-02-21 11:21:52 +0300

    DEL unused screen fun

--- app/src/main/java/net/denis/weatherapp/core/presentation/navigation/Screen.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/navigation/Screen.kt
@@ -4,11 +4,7 @@ sealed class Screen(val route: String) {
 
     object MainScreen : Screen(route = "main_screen")
 
-    object DetailScreen : Screen(route = "detail_screen/position={position}") {
-        fun passDetailPosition(position: Int): String {
-            return "detail_screen/position=$position"
-        }
-    }
+    object DetailScreen : Screen(route = "detail_screen")
 
     object FetchCityScreen:Screen(route = "fetch_city_screen")
 
