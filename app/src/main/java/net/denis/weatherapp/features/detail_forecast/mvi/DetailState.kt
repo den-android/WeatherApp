3b8c56b7f42bc73c0c10aa112f3fb1766388b974
den-android <irethegreat@yandex.ru>
2023-02-28 17:32:14 +0300

    Update DetailState.kt

--- app/src/main/java/net/denis/weatherapp/features/detail_forecast/mvi/DetailState.kt
+++ app/src/main/java/net/denis/weatherapp/features/detail_forecast/mvi/DetailState.kt
@@ -3,11 +3,11 @@ package net.denis.weatherapp.features.detail_forecast.mvi
 import androidx.annotation.Keep
 import net.denis.weatherapp.core.presentation.error.model.FailureResponse
 import net.denis.weatherapp.core.presentation.redux.State
-import net.denis.weatherapp.features.detail_forecast.model.DetailData
+import net.denis.weatherapp.features.main_forecast.model.ForecastItem
 
 @Keep
 data class DetailState(
     val isLoading: Boolean = false,
     val error: FailureResponse? = null,
-    val detailData: DetailData? = null,
+    val forecastItem: ForecastItem? = null,
 ) : State
