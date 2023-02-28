84ec0fa212965b538944c011b2984f5bd3a6d9e8
den-android <irethegreat@yandex.ru>
2023-02-28 17:13:36 +0300

    Update handleException.kt

--- app/src/main/java/net/denis/weatherapp/core/presentation/error/handlers/handleException.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/error/handlers/handleException.kt
@@ -1,6 +1,17 @@
 package net.denis.weatherapp.core.presentation.error.handlers
 
 import net.denis.weatherapp.core.presentation.error.model.FailureResponse
+import retrofit2.HttpException
 
-fun handleException(ex: Exception) =
-    FailureResponse(errMessage = ex.localizedMessage, btnTitle = "Перезапустить приложение")
+fun handleException(ex: Exception) : FailureResponse {
+    return when (ex) {
+        is HttpException -> {
+            FailureResponse(ex.localizedMessage, "Перезапустить приложение")
+        }
+
+        else -> {
+            FailureResponse(ex.localizedMessage, "Перезапустить приложение")
+        }
+    }
+}
+  //  FailureResponse(errMessage = ex.localizedMessage, btnTitle = "Перезапустить приложение")
