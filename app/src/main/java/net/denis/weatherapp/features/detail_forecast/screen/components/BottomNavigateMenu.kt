b983a6127378d455e78fc6f3ffaab3725aea5b21
den-android <irethegreat@yandex.ru>
2023-02-28 17:33:10 +0300

    Update BottomNavigateMenu.kt

--- app/src/main/java/net/denis/weatherapp/features/detail_forecast/screen/components/BottomNavigateMenu.kt
+++ app/src/main/java/net/denis/weatherapp/features/detail_forecast/screen/components/BottomNavigateMenu.kt
@@ -1,10 +1,7 @@
 package net.denis.weatherapp.features.detail_forecast.screen.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -14,6 +11,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import net.denis.weatherapp.core.presentation.ui.theme.MiddleGradientColor
+import net.denis.weatherapp.core.presentation.ui.theme.SecondaryColor
 
 @Composable
 fun BottomNavigateMenu(
@@ -23,10 +21,10 @@ fun BottomNavigateMenu(
     Column(
         modifier = modifier
             .fillMaxWidth()
-            .height(44.dp)
-            .background(MiddleGradientColor),
+            .height(50.dp)
+            .padding(bottom = 6.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Bottom,
     ) {
         FloatingActionButton(onClick = { onFabClick() }) {
             Icon(imageVector = Icons.Filled.Add, contentDescription = null)
