2b5bbee6d766c67cba0777f3817ec3a16b2c428b
den-android <irethegreat@yandex.ru>
2023-02-28 16:50:23 +0300

    add border to response text box

--- app/src/main/java/net/denis/weatherapp/features/fetch_new_city/screen/components/ResponseTextBox.kt
+++ app/src/main/java/net/denis/weatherapp/features/fetch_new_city/screen/components/ResponseTextBox.kt
@@ -1,21 +1,19 @@
 package net.denis.weatherapp.features.fetch_new_city.screen.components
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import net.denis.weatherapp.core.presentation.ui.theme.MiddleGradientColor
-import net.denis.weatherapp.core.presentation.ui.theme.PrimaryText
-import net.denis.weatherapp.core.presentation.ui.theme.ViewBackground
+import net.denis.weatherapp.core.presentation.ui.theme.*
 import net.denis.weatherapp.features.fetch_new_city.model.CityData
 
 @Composable
@@ -24,19 +22,21 @@ fun ResponseTextBox(
     cityData: CityData,
     onItemClick: () -> Unit,
 ) {
-    Card(
-        shape = RoundedCornerShape(10.dp),
-        border = BorderStroke(2.dp, MiddleGradientColor),
+    Box(
         modifier = modifier
             .fillMaxWidth()
             .height(100.dp)
             .padding(12.dp)
             .clickable { onItemClick() }
+            .clip(RoundedCornerShape(15))
+            .border(
+                border = BorderStroke(width = 1.dp, color = CellBorderColor),
+                shape = RoundedCornerShape(15)
+            )
     ) {
         Column(
             modifier = modifier
-                .fillMaxSize()
-                .background(ViewBackground),
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
         ) {
