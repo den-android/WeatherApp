73902907c8e62da79e92ca66ef11071d8081aacc
den-android <irethegreat@yandex.ru>
2023-03-01 09:44:40 +0300

    Update SearchCityTextField.kt
    add launched effect for search city

--- app/src/main/java/net/denis/weatherapp/features/fetch_new_city/screen/components/SearchCityTextField.kt
+++ app/src/main/java/net/denis/weatherapp/features/fetch_new_city/screen/components/SearchCityTextField.kt
@@ -1,18 +1,17 @@
 package net.denis.weatherapp.features.fetch_new_city.screen.components
 
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 import net.denis.weatherapp.R
 import net.denis.weatherapp.core.presentation.ui.theme.PrimaryColor
 import net.denis.weatherapp.core.presentation.ui.theme.PrimaryText
@@ -39,9 +38,7 @@ fun SearchCityTextField(
             value = textFieldValue,
             onValueChange = {
                 textFieldValue = it
-                if (it.length >= 3) {
-                    searchCity(it)
-                }
+
             },
             colors = TextFieldDefaults.textFieldColors(
                 containerColor = PrimaryColor,
@@ -63,6 +60,12 @@ fun SearchCityTextField(
 
             }
         )
+        LaunchedEffect(key1 = textFieldValue) {
+            if (textFieldValue.length >= 3) {
+                delay(300L)
+                searchCity(textFieldValue)
+            }
+        }
     }
 
 }
