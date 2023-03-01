1026cccd9f9e202b208fea4f3167b31659c9251d
den-android <irethegreat@yandex.ru>
2023-03-01 15:44:41 +0300

    ref
    removed unused compasable

--- app/src/main/java/net/denis/weatherapp/core/presentation/navigation/test/NavGraph.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/navigation/test/NavGraph.kt
@@ -1,7 +1,6 @@
 package net.denis.weatherapp.core.presentation.navigation.test
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,6 +8,7 @@ import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun NavGraph() {
+
     val navController = rememberNavController()
 
     NavHost(
@@ -16,23 +16,10 @@ fun NavGraph() {
         startDestination = CurrentForecastDirections.testCurrentForecast.destination
     ) {
 
-//        composable(CurrentForecastDirections.testCurrentForecast.destination) { backStackEntry ->
-//            val vmCurrent = hiltViewModel<TestCurrentVM>()
-//            testCurrentScreen(vm = vmCurrent)
-//        }
-//
-//        composable() { backStackEntry ->
-//            val parentEntry = remember(backStackEntry) {
-//                navController.getBackStackEntry(route = NavigationDirections.testCurrentForecast.destination)
-//            }
-//            val vmDetail = hiltViewModel<TestDetailVM>(parentEntry)
-//            testDetailForecast(vm = vmDetail)
-//        }
-//
-//        composable(FetchCityDirections) { backStackEntry ->
-//            val vmFetch = hiltViewModel<TestFetchVM>()
-//            testFetchCityScreen(vm = vmFetch)
-//        }
+        composable(CurrentForecastDirections.testCurrentForecast.destination) { backStackEntry ->
+            val vmCurrent = hiltViewModel<TestCurrentVM>()
+            testCurrentScreen(vm = vmCurrent)
+        }
 
     }
 
