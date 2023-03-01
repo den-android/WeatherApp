07270f784deee4b965eac5015347a77d889746de
den-android <irethegreat@yandex.ru>
2023-03-01 11:14:06 +0300

    Create CustomCircularProgressIndicator.kt

---
+++ app/src/main/java/net/denis/weatherapp/core/presentation/ui/components/CustomCircularProgressIndicator.kt
@@ -0,0 +1,30 @@
+package net.denis.weatherapp.core.presentation.ui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomCircularProgressIndicator(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator(
+            modifier = modifier
+                .height(160.dp),
+            color = Color.LightGray,
+        )
+    }
+}
