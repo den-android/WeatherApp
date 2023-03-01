2e833c7a27ee654a92da7cdbeb06260fe1b07817
den-android <irethegreat@yandex.ru>
2023-03-01 11:15:31 +0300

    Update MainScreen.kt
    optimize imports

--- app/src/main/java/net/denis/weatherapp/features/main_forecast/screen/MainScreen.kt
+++ app/src/main/java/net/denis/weatherapp/features/main_forecast/screen/MainScreen.kt
@@ -1,22 +1,20 @@
 package net.denis.weatherapp.features.main_forecast.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import net.denis.weatherapp.R
 import net.denis.weatherapp.core.presentation.error.ErrorAlertDialog
 import net.denis.weatherapp.core.presentation.ui.components.CustomCircularProgressIndicator
