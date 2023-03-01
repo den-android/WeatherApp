01010704a7edc285be0d4e6dd81c3e3c08c2a47d
den-android <irethegreat@yandex.ru>
2023-03-01 11:24:29 +0300

    add new exceptions handler

--- app/src/main/java/net/denis/weatherapp/core/presentation/error/handlers/handleException.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/error/handlers/handleException.kt
@@ -2,11 +2,21 @@ package net.denis.weatherapp.core.presentation.error.handlers
 
 import net.denis.weatherapp.core.presentation.error.model.FailureResponse
 import retrofit2.HttpException
+import java.net.ConnectException
+import java.net.UnknownHostException
 
 fun handleException(ex: Exception) : FailureResponse {
     return when (ex) {
         is HttpException -> {
             FailureResponse(ex.localizedMessage, "Перезапустить приложение")
+        }
+
+        is ConnectException -> {
+            FailureResponse(errMessage = "Отсутствует подключение к сети Интернет", "Перезагрузить страницу")
+        }
+
+        is UnknownHostException -> {
+            FailureResponse(errMessage = "Отсутствует подключение к сети Интернет", "Перезагрузить")
         }
 
         else -> {
