1215bbf1639c87e028b0f7d042868bd488d68784
den-android <irethegreat@yandex.ru>
2023-03-01 14:43:06 +0300

    Create NavGraph.kt

---
+++ app/src/main/java/net/denis/weatherapp/core/presentation/navigation/NavGraph.kt
@@ -0,0 +1,13 @@
+package net.denis.weatherapp.core.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavGraph(
+
+) {
+
+
+}
