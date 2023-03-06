4d857b1600516b2e8687759ee77d42e7c7471915
den-android <irethegreat@yandex.ru>
2023-03-06 17:34:07 +0300

    Update MainDataMiddleware.kt

--- app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainDataMiddleware.kt
+++ app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainDataMiddleware.kt
@@ -1,12 +1,16 @@
 package net.denis.weatherapp.features.main_forecast.mvi
 
+import android.util.Log
 import net.denis.weatherapp.core.data.datasource.remote.dto.weather_forecast.mapToForecastData
 import net.denis.weatherapp.core.data.interfaces.IWeatherRepository
-import net.denis.weatherapp.core.presentation.error.handlers.handleException
-import net.denis.weatherapp.core.presentation.error.handlers.handleHttpCode
 import net.denis.weatherapp.core.presentation.redux.Middleware
 import net.denis.weatherapp.core.presentation.redux.Store
+import net.denis.weatherapp.core.util.OnExceptionError
+import net.denis.weatherapp.core.util.OnHttpError
 import net.denis.weatherapp.core.util.network.NetworkResult
+import retrofit2.HttpException
+import java.net.SocketTimeoutException
+import java.net.UnknownHostException
 
 class MainDataMiddleware(
     private val weatherRepository: IWeatherRepository,
@@ -19,7 +23,26 @@ class MainDataMiddleware(
     ) {
         when (action) {
             is MainAction.FetchForecast -> {
-                fetchForecast(lat = action.lat, lon = action.lon, store = store)
+                fetchForecast(lat = 47.2213858, lon = 39.7114196, store = store)
+            }
+
+            is MainAction.FixError -> {
+                when (currentState.failureResponse) {
+                    OnHttpError.Code1 -> store.dispatch(MainAction.FetchForecast) // restarted fetch
+                    OnHttpError.Code2 -> {} // Does nothing
+                    OnHttpError.Code401 -> store.dispatch(MainAction.FetchForecast) // restarted fetch
+
+                    OnExceptionError.ExUnknownHostException -> {
+                        Log.d("Logging", "222")
+                        store.dispatch(MainAction.FetchForecast)
+                    }
+//                    OnExceptionError.ExSocketTimeoutException -> {
+//                        Log.d("Logging", "${currentState.failureResponse}")
+//                        store.dispatch(MainAction.FetchForecast)
+//                    }
+                    null -> Log.d("Logging", "MainAction.FixError -> null")
+                }
+
             }
 
             else -> currentState
@@ -35,15 +58,43 @@ class MainDataMiddleware(
         weatherRepository.fetchForecast(lat = lat, lon = lon).collect { response ->
             when (response) {
                 is NetworkResult.Success -> {
-                    store.dispatch(MainAction.ForecastLoaded(forecastData = response.data.mapToForecastData()))
+                    store.dispatch(
+                        MainAction.ForecastLoaded(forecastData = response.data.mapToForecastData())
+                    )
                 }
                 is NetworkResult.Failure -> {
-                    store.dispatch(MainAction.ShowError(handleHttpCode(response.code)))
+                    handlerHttpCode(response.code, store)
                 }
                 is NetworkResult.Exception -> {
-                    store.dispatch(MainAction.ShowError(handleException(response.e)))
+                    Log.d("Logging", "AAA")
+                    handlerException(response.e, store)
                 }
             }
+        }
+    }
+
+    private suspend fun handlerHttpCode(code: Int, store: Store<MainState, MainAction>) {
+        when (code) {
+            1 -> {
+                store.dispatch(MainAction.ShowError(OnHttpError.Code1))
+            }
+            2 -> {
+                store.dispatch(MainAction.ShowError(OnHttpError.Code2))
+            }
+            401 -> {
+                store.dispatch(MainAction.ShowError(OnHttpError.Code401))
+            }
+        }
+    }
+
+    private suspend fun handlerException(ex: Exception, store: Store<MainState, MainAction>) {
+        when (ex) {
+            is UnknownHostException -> {
+                store.dispatch(MainAction.ShowError(OnExceptionError.ExUnknownHostException))
+            }
+//            is SocketTimeoutException -> {
+//                store.dispatch(MainAction.ShowError(OnExceptionError.ExSocketTimeoutException))
+//            }
         }
     }
 
