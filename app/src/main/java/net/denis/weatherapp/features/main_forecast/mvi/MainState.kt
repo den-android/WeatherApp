7660cfacd80d922596faa0cbcd3b78a28d569e1f
den-android <irethegreat@yandex.ru>
2023-03-06 17:32:45 +0300

    Update MainState.kt

--- app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainState.kt
+++ app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainState.kt
@@ -1,13 +1,13 @@
 package net.denis.weatherapp.features.main_forecast.mvi
 
 import androidx.annotation.Keep
-import net.denis.weatherapp.core.presentation.error.model.FailureResponse
+import net.denis.weatherapp.core.util.FailureResponse
 import net.denis.weatherapp.core.presentation.redux.State
 import net.denis.weatherapp.features.main_forecast.model.ForecastData
 
 @Keep
 data class MainState(
     val isLoading: Boolean = false,
-    val error: FailureResponse? = null,
+    val failureResponse: FailureResponse? = null,
     val forecastData: ForecastData? = null
 ) : State
