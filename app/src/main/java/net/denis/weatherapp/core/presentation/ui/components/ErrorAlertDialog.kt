eb96977e3de397f4c2ca6c6e680748c9f51c2ad6
den-android <irethegreat@yandex.ru>
2023-03-06 17:31:26 +0300

    Update ErrorAlertDialog.kt

--- app/src/main/java/net/denis/weatherapp/core/presentation/ui/components/ErrorAlertDialog.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/ui/components/ErrorAlertDialog.kt
@@ -1,5 +1,6 @@
 package net.denis.weatherapp.core.presentation.ui.components
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,6 +12,8 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -26,58 +29,65 @@ fun ErrorAlertDialog(
     modifier: Modifier = Modifier,
     onActionErrorClick: () -> Unit,
     onExitClick: () -> Unit,
-    failureResponse: FailureResponse
+    failureResponse: FailureResponse,
 ) {
-    Dialog(
-        onDismissRequest = { onActionErrorClick() },
-        properties = DialogProperties(
-            dismissOnBackPress = false, dismissOnClickOutside = false
-        )
-    ) {
-        Card(
-            shape = RoundedCornerShape(10.dp),
-            modifier = modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-        ) {
-            Column(
-                Modifier
-                    .fillMaxWidth()
-                    .background(Color.White)
-            ) {
-                Text(
-                    text = failureResponse.errMessage,
-                    textAlign = TextAlign.Center,
-                    modifier = modifier.padding(8.dp), fontSize = 20.sp
-                )
-                Row(Modifier.padding(top = 10.dp)) {
-                    OutlinedButton(
-                        onClick = { onExitClick() },
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                            .weight(1f)
-                    ) {
-                        Text(text = "Закрыть")
-                    }
+    var alert = remember { mutableStateOf(failureResponse.alertState) }
 
-                    Button(
-                        onClick = { onActionErrorClick() },
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(8.dp)
-                            .weight(1f)
-                    ) {
-                        Text(
-                            overflow = TextOverflow.Ellipsis,
-                            maxLines = 1,
-                            text = failureResponse.btnTitle,
-                        )
+    if (alert.value) {
+        Dialog(
+            onDismissRequest = {
+                onActionErrorClick()
+                alert.value = false
+            },
+            properties = DialogProperties(
+                dismissOnBackPress = false, dismissOnClickOutside = false
+            )
+        ) {
+            Card(
+                shape = RoundedCornerShape(10.dp),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .background(Color.White)
+                ) {
+                    Text(
+                        text = failureResponse.errMessage,
+                        textAlign = TextAlign.Center,
+                        modifier = modifier.padding(8.dp), fontSize = 20.sp
+                    )
+                    Row(Modifier.padding(top = 10.dp)) {
+                        OutlinedButton(
+                            onClick = { onExitClick() },
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .weight(1f)
+                        ) {
+                            Text(text = "Закрыть")
+                        }
+
+                        Button(
+                            onClick = { onActionErrorClick() },
+                            modifier = modifier
+                                .fillMaxWidth()
+                                .padding(8.dp)
+                                .weight(1f)
+                        ) {
+                            Text(
+                                overflow = TextOverflow.Ellipsis,
+                                maxLines = 1,
+                                text = failureResponse.btnTitle,
+                            )
+                        }
                     }
                 }
-
-
             }
         }
+
     }
+
 }
