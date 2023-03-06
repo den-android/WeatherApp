7e73bf62d4c4063a7fd9b23ef9f4b868cf939cdb
den-android <irethegreat@yandex.ru>
2023-03-06 11:40:45 +0300

    ref

--- app/src/main/java/net/denis/weatherapp/core/presentation/error/model/ErrorType.kt
+++
@@ -1,6 +0,0 @@
-package net.denis.weatherapp.core.presentation.error.model
-
-sealed class ErrorType {
-    data class HttpError(val httpErrorResponse: HttpErrorResponse) : ErrorType()
-    data class OnExceptionError(val exMessage: String) : ErrorType()
-}
--- app/src/main/java/net/denis/weatherapp/core/presentation/error/model/FailureResponse.kt
+++
@@ -1,9 +0,0 @@
-package net.denis.weatherapp.core.presentation.error.model
-
-import androidx.annotation.Keep
-
-@Keep
-data class FailureResponse(
-    val errMessage: String,
-    val btnTitle: String
-)
