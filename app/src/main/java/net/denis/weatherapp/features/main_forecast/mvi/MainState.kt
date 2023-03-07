83ec0379fe0526ad1c922436738c3c196382964b
den-android <irethegreat@yandex.ru>
2023-03-07 11:02:56 +0300

    Update MainState.kt

--- app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainState.kt
+++ app/src/main/java/net/denis/weatherapp/features/main_forecast/mvi/MainState.kt
@@ -1,8 +1,8 @@
 package net.denis.weatherapp.features.main_forecast.mvi
 
 import androidx.annotation.Keep
-import net.denis.weatherapp.core.util.FailureResponse
 import net.denis.weatherapp.core.presentation.redux.State
+import net.denis.weatherapp.core.util.FailureResponse
 import net.denis.weatherapp.features.main_forecast.model.ForecastData
 
 @Keep
