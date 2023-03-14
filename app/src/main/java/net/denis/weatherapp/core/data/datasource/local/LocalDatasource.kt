f28f5951a3653175187e2d55f79ea3d013fccfd8
den-android <irethegreat@yandex.ru>
2023-03-14 14:26:43 +0300

    add protobuf

--- app/src/main/java/net/denis/weatherapp/core/data/datasource/local/LocalDatasource.kt
+++ app/src/main/java/net/denis/weatherapp/core/data/datasource/local/LocalDatasource.kt
@@ -1,12 +1,11 @@
 package net.denis.weatherapp.core.data.datasource.local
 
+import kotlinx.coroutines.CoroutineScope
 import net.denis.weatherapp.core.data.interfaces.ILocalDatasource
 import javax.inject.Inject
 
 class LocalDatasource @Inject constructor(
-
+    private val scopeIO: CoroutineScope
 ) : ILocalDatasource {
-
-
 
 }
