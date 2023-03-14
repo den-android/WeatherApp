3f091559d62b97047dc5edb81299c8dbcd2adfc8
den-android <irethegreat@yandex.ru>
2023-03-14 15:42:45 +0300

    Update CurrentForecastDirections.kt

--- app/src/main/java/net/denis/weatherapp/core/presentation/navigation/directions/CurrentForecastDirections.kt
+++ app/src/main/java/net/denis/weatherapp/core/presentation/navigation/directions/CurrentForecastDirections.kt
@@ -2,8 +2,6 @@ package net.denis.weatherapp.core.presentation.navigation.directions
 
 import androidx.navigation.NamedNavArgument
 import net.denis.weatherapp.core.presentation.navigation.INavigationCommand
-import net.denis.weatherapp.core.util.Constants.PARAM_LAT
-import net.denis.weatherapp.core.util.Constants.PARAM_LON
 
 object CurrentForecastDirections {
 
@@ -14,10 +12,7 @@ object CurrentForecastDirections {
 
     val CurrentForecast = object : INavigationCommand {
         override val arguments = emptyList<NamedNavArgument>()
-        override val destination = "current_forecast?=$PARAM_LAT={$PARAM_LAT}?=$PARAM_LON={$PARAM_LON}"
-    }
-    fun sendParamToDetailForecast() {
-
+        override val destination = "current_forecast"
     }
 
 }
