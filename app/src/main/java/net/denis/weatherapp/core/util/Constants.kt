cc9a243e627edf7b2a5d7e7306b8fae71d514f0b
den-android <irethegreat@yandex.ru>
2023-03-15 11:49:27 +0300

    Update Constants.kt

--- app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
+++ app/src/main/java/net/denis/weatherapp/core/util/Constants.kt
@@ -1,6 +1,6 @@
 package net.denis.weatherapp.core.util
 object Constants {
     const val API_KEY = "b05865d24d90b1dbccfb3ced2627b4e9"
-    const val DEFAULT_LAT = 1.0
-    const val DEFAULT_LON = 0.1
+    const val DEFAULT_LAT = 47.2213858
+    const val DEFAULT_LON = 39.7114196
 }
