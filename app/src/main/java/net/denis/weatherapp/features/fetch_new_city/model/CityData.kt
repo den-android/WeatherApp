861ef1077da78eb63dedcae7819c24186f1df001
den-android <irethegreat@yandex.ru>
2023-03-15 11:52:54 +0300

    Update CityData.kt

--- app/src/main/java/net/denis/weatherapp/features/fetch_new_city/model/CityData.kt
+++ app/src/main/java/net/denis/weatherapp/features/fetch_new_city/model/CityData.kt
@@ -1,37 +1,12 @@
 package net.denis.weatherapp.features.fetch_new_city.model
 
-import android.os.Parcel
-import android.os.Parcelable
 import androidx.annotation.Keep
+import kotlinx.serialization.Serializable
 
 @Keep
+@Serializable
 data class CityData(
     val name: String,
     val lat: Double,
     val lon: Double
-) : Parcelable {
-
-    constructor(parcel: Parcel) : this(
-        name = parcel.readString() ?: "",
-        lat = parcel.readDouble(),
-        lon = parcel.readDouble()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeDouble(lat)
-        parcel.writeDouble(lon)
-    }
-
-    override fun describeContents(): Int = 0
-
-    companion object CREATOR : Parcelable.Creator<CityData> {
-        override fun createFromParcel(parcel: Parcel): CityData {
-            return CityData(parcel)
-        }
-
-        override fun newArray(size: Int): Array<CityData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+)
